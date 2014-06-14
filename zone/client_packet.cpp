b92e1fb30a22672b1b67635e632c7e8111a26d7a
Scott Davis <me@sdavis.info>
2014-06-14 08:11:57 +0000

    whitespace to tab oops

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -7142,7 +7142,7 @@ void Client::Handle_OP_Animation(const EQApplicationPacket *app)
 	Animation_Struct *s = (Animation_Struct *) app->pBuffer;
 
 	//might verify spawn ID, but it wouldent affect anything
-    Animation action = static_cast<Animation>(s->action);
+	Animation action = static_cast<Animation>(s->action);
 	DoAnim(action, s->value);
 
 	return;
