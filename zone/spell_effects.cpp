3e5375860908c15a59da5d98fb431e826c7abeb5
Scott Davis <me@sdavis.info>
2014-06-21 04:11:32 +0000

    modifyed SetInvisible to take a flag is we should send the invis packet so that charm breaks correctly on invis vs undead and invis vs animals

--- zone/spell_effects.cpp
+++ zone/spell_effects.cpp
@@ -565,6 +565,7 @@ bool Mob::SpellEffect(Mob* caster, uint16 spell_id, float partial)
 				snprintf(effect_desc, _EDLEN, "Invisibility to Animals");
 #endif
 				invisible_animals = true;
+                SetInvisible(spell.base[i], false);
 				break;
 			}
 
@@ -575,6 +576,7 @@ bool Mob::SpellEffect(Mob* caster, uint16 spell_id, float partial)
 				snprintf(effect_desc, _EDLEN, "Invisibility to Undead");
 #endif
 				invisible_undead = true;
+                SetInvisible(spell.base[i], false);
 				break;
 			}
 			case SE_SeeInvis:
@@ -3551,7 +3553,7 @@ void Mob::BuffFadeBySlot(int slot, bool iRecalcBonuses)
 			case SE_Invisibility2:
 			case SE_Invisibility:
 			{
-				SetInvisible(0);
+				SetInvisible(DROP_INVISIBLE);
 				break;
 			}
 
